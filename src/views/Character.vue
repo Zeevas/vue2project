96e4794c1d2ebfd3d890e19c770b9519bbd2bdd2
Zeevas <zevdestroyer@gmail.com>
2020-11-16 23:13:09 +0300

    first commit

---
+++ src/views/Character.vue
@@ -0,0 +1,13 @@
+<template>
+  <div >
+    character
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Character',
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    zalas
+        <character-block v-for="character in characters" :key="character.id" />
+  </div>
+</template>
+
+<script>
+
+import CharacterBlock from '@/components/CharacterBlock.vue';
+
+export default {
+  name: 'home',
+  components: {
+  CharacterBlock
+  },
+  data() {
+    return {
+      currentPage: 1,
+    }
+
+  },
+  created() {
+    this.$store.dispatch('fetchCharacters',this.currentPage);
+  },
+  computed:{
+    characters() {
+      return this.$store.getters['getCharactersByPage'](this.currentPage);
+    }
+  }
+}
+</script>
